555c842edfbd5a0af131ce74b62d2704fefe6d8b
dhe10 <daniel.hembery@skybettingandgaming.com>
2023-02-08 11:13:53 +0000

    INITIAL COMMIT

---
+++ src/main/java/com/fadedink/fadedspringbootbe/FadedSpringbootBeApplication.java
@@ -0,0 +1,13 @@
+package com.fadedink.fadedspringbootbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FadedSpringbootBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FadedSpringbootBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fadedink/fadedspringbootbe/FadedSpringbootBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadedink.fadedspringbootbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FadedSpringbootBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
